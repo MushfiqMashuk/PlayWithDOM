1d467d58e8d177f78fa2e2375c62e2491254427c
Mushfiq Mashuk <mushfiqmashuk96@gmail.com>
2021-08-15 02:02:14 +0600

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+console.dir(document);
 console.log(document.title);
-console.log(document.title);
+console.log(document.URL);
+console.log(document.domain);
+console.dir(document.head);
+console.dir(document.all);
