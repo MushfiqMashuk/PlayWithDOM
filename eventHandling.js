71c38f65af7191d3e0016e9e1613035f82a8655b
Mushfiq Mashuk <mushfiqmashuk96@gmail.com>
2021-08-17 23:16:22 +0600

    initial commit

--- eventHandling.js
+++ eventHandling.js
@@ -5,3 +5,19 @@ city.addEventListener('mousedown', (event) => {
 });
 
 //**mouseover event works for the children inside of an element. but mouseenter only works for the element and fires event when mouse enters upon the element. */ 
+
+/*
+keypress, keyup, kyedown, focus, blur, cut, paste, input,
+
+form event: submit
+*/
+
+const input = document.querySelector('input[id="input"]');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    //console.log(event.key);
+    event.preventDefault();
+    console.log(event.target);
+    
+});
